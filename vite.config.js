43a72393e29e0d9b0af8f2133f2fa9199001f2dd
Sahidannopal-15 <muhammadnaufalsahidan123@gmail.com>
2025-12-25 16:43:56 +0700

    komat

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [tailwindcss()],
-   base: '/HashX15/',
-    build: {
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github' ? '/HashX15/' : '/',
+  plugins: [react(), tailwindcss()],
+  build: {
     outDir: 'dist'
   }
-})
+}))
